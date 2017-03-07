2abde1d7c21c57e92f2f25a2ac8ec5a5bf90638c
mlgantt <mgantt@sapient.com>
2017-03-07 15:07:53 -0800

    updated the bot to speak

--- index.js
+++ index.js
@@ -18,9 +18,28 @@ app.get('/', function (req, res) {
     res.send('Hello world, I am a chat bot')
 })
 
+function sendTextMessage(sender, text) {
+    let messageData = { text:text }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
+
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
-    if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+    if (req.query['hub.verify_token'] === 'supersecretpassword') {
         res.send(req.query['hub.challenge'])
     }
     res.send('Error, wrong token')
@@ -30,3 +49,18 @@ app.get('/webhook/', function (req, res) {
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
 })
+
+app.post('/webhook/', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging
+    for (let i = 0; i < messaging_events.length; i++) {
+        let event = req.body.entry[0].messaging[i]
+        let sender = event.sender.id
+        if (event.message && event.message.text) {
+            let text = event.message.text
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
+    }
+    res.sendStatus(200)
+})
+
+const token = "EAAZAEXqKtzwABAK3JggZAaV4bmZBPF2JqlP1X7O02cX8uYzmNOQOKCdMQly0jenYQacWwHbsseoQ1tTnCii0IKj61ZAhSYHtxudTIqHYOLrZAGwhk8huOZAdJoZC8uaHmxZBvjVYjXxU5nIXZCpNup0iWq3B1ZBhqB7ut9LVZCeNC9s3wZDZD"
