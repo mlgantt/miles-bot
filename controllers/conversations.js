a8de4cfd82e555b8d621bff66599acd34f3d84aa
mlgantt <mgantt@sapient.com>
2017-03-10 16:42:32 -0800

    pizzatime

--- controllers/conversations.js
+++ controllers/conversations.js
@@ -10,7 +10,7 @@ module.exports = function(controller) {
     })
 
 
-    controller.hears(['cookies'], 'message_received', function(bot, message) {
+    controller.hears(['weather'], 'message_received', function(bot, message) {
 
         bot.startConversation(message, function(err, convo) {
 
@@ -23,38 +23,64 @@ module.exports = function(controller) {
     });
 
 
-    controller.hears(["weather"],'message_received',function(bot, message) {
+    controller.hears(["weather"], 'message_received', function(bot, message) {
 
-        var txt = message.text;
-        txt = txt.toLowerCase().replace('weather ', '');
-        var city = txt.split(',')[0].trim().replace(' ', '_');
-        var state = txt.split(',')[1].trim();
+        // var txt = message.text;
+        // console.log(txt)
+        // txt = txt.toLowerCase().replace('weather ', '');
+        // var city = txt.split(',')[0].trim().replace(' ', '_');
+        // var state = txt.split(',')[1].trim();
 
-        console.log(city + ', ' + state);
-        var url = '/api/' + key + '/forecast/q/state/city.json'
-        url = url.replace('state', state);
-        url = url.replace('city', city);
+        // console.log(city + ', ' + state);
+        // var url = '/api/' + key + '/forecast/q/state/city.json'
+        // url = url.replace('state', state);
+        // url = url.replace('city', city);
 
-        http.get({
-            host: 'api.wunderground.com',
-            path: url
-        }, function(response) {
-            var body = '';
-            response.on('data', function(d) {
-                body += d;
-            })
-            response.on('end', function() {
-                var data = JSON.parse(body);
-                var days = data.forecast.simpleforecast.forecastday;
-                for (i = 0; i < days.length; i++) {
-                    bot.reply(message, days[i].date.weekday +
-                        ' high: ' + days[i].high.fahrenheit +
-                        ' low: ' + days[i].low.fahrenheit +
-                        ' condition: ' + days[i].conditions);
-                    bot.reply(message, days[i].icon_url);
-                }
-            })
-        })
+        // http.get({
+        //     host: 'api.wunderground.com',
+        //     path: url
+        // }, function(response) {
+        //     var body = '';
+        //     response.on('data', function(d) {
+        //         body += d;
+        //     })
+        //     response.on('end', function() {
+        //         var data = JSON.parse(body);
+        //         var days = data.forecast.simpleforecast.forecastday;
+        //         for (i = 0; i < days.length; i++) {
+        //             bot.reply(message, days[i].date.weekday +
+        //                 ' high: ' + days[i].high.fahrenheit +
+        //                 ' low: ' + days[i].low.fahrenheit +
+        //                 ' condition: ' + days[i].conditions);
+        //             bot.reply(message, days[i].icon_url);
+        //         }
+        //     })
+        // })
+    });
+
+    controller.hears(['pizzatime'], 'message_received', function(bot, message) {
+        var askFlavor = function(err, convo) {
+            convo.ask('What flavor of pizza do you want?', function(response, convo) {
+                convo.say('Awesome.');
+                askSize(response, convo);
+                convo.next();
+            });
+        };
+        var askSize = function(response, convo) {
+            convo.ask('What size do you want?', function(response, convo) {
+                convo.say('Ok.')
+                askWhereDeliver(response, convo);
+                convo.next();
+            });
+        };
+        var askWhereDeliver = function(response, convo) {
+            convo.ask('So where do you want it delivered?', function(response, convo) {
+                convo.say('Ok! Good bye.');
+                convo.next();
+            });
+        };
+
+        bot.startConversation(message, askFlavor);
     });
 
     // user says anything else
