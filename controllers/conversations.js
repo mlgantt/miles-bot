0a7f7829bf70a8f2c17ed240b6c00ea283b7a7d1
mlgantt <mgantt@sapient.com>
2017-03-10 16:23:38 -0800

    added weather

--- controllers/conversations.js
+++ controllers/conversations.js
@@ -22,7 +22,41 @@ module.exports = function(controller) {
         });
     });
 
-    
+
+    controller.hears(["weather"], ["mention", "direct_mention", "direct_message"], function(bot, message) {
+
+        var txt = message.text;
+        txt = txt.toLowerCase().replace('weather ', '');
+        var city = txt.split(',')[0].trim().replace(' ', '_');
+        var state = txt.split(',')[1].trim();
+
+        console.log(city + ', ' + state);
+        var url = '/api/' + key + '/forecast/q/state/city.json'
+        url = url.replace('state', state);
+        url = url.replace('city', city);
+
+        http.get({
+            host: 'api.wunderground.com',
+            path: url
+        }, function(response) {
+            var body = '';
+            response.on('data', function(d) {
+                body += d;
+            })
+            response.on('end', function() {
+                var data = JSON.parse(body);
+                var days = data.forecast.simpleforecast.forecastday;
+                for (i = 0; i < days.length; i++) {
+                    bot.reply(message, days[i].date.weekday +
+                        ' high: ' + days[i].high.fahrenheit +
+                        ' low: ' + days[i].low.fahrenheit +
+                        ' condition: ' + days[i].conditions);
+                    bot.reply(message, days[i].icon_url);
+                }
+            })
+        })
+    });
+
     // user says anything else
     controller.hears('(.*)', 'message_received', function(bot, message) {
         bot.reply(message, 'you said ' + message.match[1])
