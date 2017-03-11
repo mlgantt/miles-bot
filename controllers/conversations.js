5e8ac6f1f2149420341c18b812b322d565d5395c
mlgantt <mgantt@sapient.com>
2017-03-10 16:10:41 -0800

    added conversation functionality

--- controllers/conversations.js
+++ controllers/conversations.js
@@ -1,17 +1,28 @@
+module.exports = function(controller) {
+    // this is triggered when a user clicks the send-to-messenger plugin
+    controller.on('facebook_optin', function(bot, message) {
+        bot.reply(message, 'Welcome, friend')
+    })
 
-module.exports = function (controller) {
-  // this is triggered when a user clicks the send-to-messenger plugin
-  controller.on('facebook_optin', function (bot, message) {
-    bot.reply(message, 'Welcome, friend')
-  })
+    // user said hello
+    controller.hears(['hello'], 'message_received', function(bot, message) {
+        bot.reply(message, 'Hey there.')
+    })
 
-  // user said hello
-  controller.hears(['hello'], 'message_received', function (bot, message) {
-    bot.reply(message, 'Hey there.')
-  })
+    // user says anything else
+    controller.hears('(.*)', 'message_received', function(bot, message) {
+        bot.reply(message, 'you said ' + message.match[1])
+    })
 
-  // user says anything else
-  controller.hears('(.*)', 'message_received', function (bot, message) {
-    bot.reply(message, 'you said ' + message.match[1])
-  })
+    controller.hears(['cookies'], 'message_received', function(bot, message) {
+
+        bot.startConversation(message, function(err, convo) {
+
+            convo.say('Did someone say cookies!?!!');
+            convo.ask('What is your favorite type of cookie?', function(response, convo) {
+                convo.say('Golly, I love ' + response.text + ' too!!!');
+                convo.next();
+            });
+        });
+    });
 }
