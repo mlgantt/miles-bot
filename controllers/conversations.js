3ff63524e525faf1b52fcecccbeb1f5fb2f682c2
mlgantt <mgantt@sapient.com>
2017-03-10 19:23:26 -0800

    i removed the stupid button

--- controllers/conversations.js
+++ controllers/conversations.js
@@ -10,12 +10,12 @@ module.exports = function(controller) {
     })
 
 
-    controller.hears(['cookies'], 'message_received', function(bot, message) {
+    controller.hears(['goldfish'], 'message_received', function(bot, message) {
 
         bot.startConversation(message, function(err, convo) {
 
-            convo.say('Did someone say cookies!?!!');
-            convo.ask('What is your favorite type of cookie?', function(response, convo) {
+            convo.say('Did someone say goldfish!?!!');
+            convo.ask('What is your favorite type of goldfish?', function(response, convo) {
                 convo.say('Golly, I love ' + response.text + ' too!!!');
                 convo.next();
             });
